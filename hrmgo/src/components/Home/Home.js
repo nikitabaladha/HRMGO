46b29987581f4a47b41a3af5e07e951d8cc972e8
nikitabaladha <nikitabaladha1996@gmail.com>
2024-10-29 09:23:02 +0530

    Initialization of HRMGO Project with initial react setup and folder structure

---
+++ hrmgo/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+import Sidebar from "../Sidebar/Sidebar.js";
+
+const Home = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Home;
