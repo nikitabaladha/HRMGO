60d39e5c854fb2e6b9a45eafe5bae7b521572d4d
nikitabaladha <nikitabaladha1996@gmail.com>
2024-11-06 15:23:41 +0530

    Meeting page done with backend connection

--- hrmgo/src/components/DashboardMain/MeetingClockCalender/Meeting.js
+++ hrmgo/src/components/DashboardMain/MeetingClockCalender/Meeting.js
@@ -1,44 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import getAPI from "../../../api/getAPI.js";
 
 const Meeting = () => {
-  // Data for each meeting
-  const meetings = [
-    { title: "New Technology", date: "Oct 2, 2023", time: "3:20 PM" },
-    { title: "Team Meeting", date: "Sep 6, 2022", time: "5:00 PM" },
-    { title: "Event Related", date: "Dec 11, 2023", time: "2:25 PM" },
-    { title: "Company Rules", date: "Nov 18, 2023", time: "4:30 PM" },
-  ];
+  const [meetings, setMeetings] = useState([]);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const month = date.toLocaleString("default", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+  }
+
+  useEffect(() => {
+    const fetchMeeting = async () => {
+      try {
+        const response = await getAPI(`/meeting-get-all`, {}, true);
+        if (
+          !response.hasError &&
+          response.data &&
+          Array.isArray(response.data.data)
+        ) {
+          setMeetings(response.data.data);
+          console.log("Meetings fetched successfully", response.data.data);
+        } else {
+          console.error("Invalid response format or error in response");
+        }
+      } catch (err) {
+        console.error("Error fetching meetings:", err);
+      }
+    };
+
+    fetchMeeting();
+  }, []);
 
   return (
-    <>
-      <div className="card">
-        <div className="card-header card-body table-border-style">
-          <h5>Meeting schedule</h5>
-        </div>
-        <div className="card-body" style={{ height: 324, overflow: "auto" }}>
-          <div className="table-responsive">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Date</th>
-                  <th>Time</th>
+    <div className="card">
+      <div className="card-header card-body table-border-style">
+        <h5>Meeting Schedule</h5>
+      </div>
+      <div className="card-body" style={{ height: 324, overflow: "auto" }}>
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Date</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody className="list">
+              {meetings.map((meeting) => (
+                <tr key={meeting._id}>
+                  <td>{meeting.title}</td>
+                  <td>{formatDate(meeting.date)}</td>
+                  <td>{meeting.time}</td>
                 </tr>
-              </thead>
-              <tbody className="list">
-                {meetings.map((meeting, index) => (
-                  <tr key={index}>
-                    <td>{meeting.title}</td>
-                    <td>{meeting.date}</td>
-                    <td>{meeting.time}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
